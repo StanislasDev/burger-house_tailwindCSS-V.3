3a844113ff6cbc6655c02dcf20a8c642203d9042
Stanislas <simostanley91@gmail.com>
2025-03-26 10:02:59 +0100

    fin section#home << section#category

---
+++ src/scripts/main.js
@@ -0,0 +1,131 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const closeIcon = document.querySelector(".nav-close");
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    // navMenu.style.top = "-25vh";
+    navMenu.classList.add('hidden');
+  });
+});
+
+closeIcon.addEventListener("click", () => {
+  // navMenu.style.top = "-25vh";
+  navMenu.classList.add('hidden');
+});
+
+hamburger.addEventListener("click", () => {
+  // navMenu.style.top = "0vh";
+  navMenu.classList.remove('hidden');
+});
+
+// const hamburger = document.getElementById('hamburger');
+// const navMenu = document.getElementById('nav-menu');
+// const closeIcon = document.getElementById('nav-close');
+// const navLink = document.querySelectorAll('.nav__link');
+
+// const hideNavMenu = () => {
+//   navMenu.classList.add('hidden');
+// };
+
+// const showNavMenu = () => {
+//   navMenu.classList.remove('hidden');
+// };
+
+// navLink.forEach((link) => {
+//   link.addEventListener('click', hideNavMenu);
+// });
+
+// closeIcon.addEventListener('click', hideNavMenu);
+
+// hamburger.addEventListener('click', showNavMenu);
+
+/*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
+const tabs = document.querySelectorAll(".tabs_wrap li");
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const beverages = document.querySelectorAll(".beverage");
+const snacks = document.querySelectorAll(".snack");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    tab.classList.add("active");
+    const tabval = tab.getAttribute("data-tabs");
+    console.log(tabval);
+
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    switch (tabval) {
+      case "food":
+        foods.forEach((item) => {
+          item.style.display = "block";
+        });
+        break;
+      case "snack":
+        snacks.forEach((item) => {
+          item.style.display = "block";
+        });
+        break;
+      case "beverage":
+        beverages.forEach((item) => {
+          item.style.display = "block";
+        });
+        break;
+      default:
+        all.forEach((item) => {
+          item.style.display = "block";
+        });
+        break;
+    }
+    // if (tabval == "food") {
+    //   foods.forEach((item) => {
+    //     item.style.display = "block";
+    //   });
+    // } else if (tabval == "snack") {
+    //   snacks.forEach((item) => {
+    //     item.style.display = "block";
+    //   });
+    // } else if (tabval == "beverage") {
+    //   beverages.forEach((item) => {
+    //     item.style.display = "block";
+    //   });
+    // } else {
+    //   all.forEach((item) => {
+    //     item.style.display = "block";
+    //   });
+    // }
+  });
+});
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-4");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-4");
+  }
+};
+window.addEventListener("scroll", scrollUp);
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 50) {
+    header.classList.add("border-b", "border-secondaryColor");
+  } else {
+    header.classList.remove("border-b", "border-secondaryColor");
+  }
+};
+window.addEventListener("scroll", scrollHeader);
